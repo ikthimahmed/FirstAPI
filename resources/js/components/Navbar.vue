1e762a38e8f191ed436df0d82dcf8ec0d0da8099
Ikthimal Ahmed <Ikthimal@aasandha.net>
2019-10-28 09:26:51 +0500

    First commit

---
+++ resources/js/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <nav class="navbar navbar-expand-sm navbar-dark bg-info mb-2">
+    <div class="container">
+      <a href="/" class="navbar-brand text-white">BLOG</a>
+    </div>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto ">
+      <li class>
+        <a class="nav-link text-white" href="/">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-white" href="/View">Articles</a>
+      </li>
+    </ul>
+  </div>
+  </nav>
+</template>
---
+++ resources/js/components/View.vue
@@ -0,0 +1,46 @@
+<template>
+<div>
+    <div v-for="item in articles" :key="item.id">
+    <h5>{{ item.title }}</h5>
+    <p>{{ item.body }}</p>
+    </div>
+    
+</div>
+
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    name:'articles',
+    data() {
+        return {
+            articles: null
+        }
+    },
+
+created(){
+    this.getArticles()
+},
+
+methods: {
+    getArticles(){
+        axios
+        .get('/api/articles')
+        .then(res => {
+            const {data:{data}} = res
+            this.articles = data
+            console.log(res)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+
+            console.log('test')
+
+    }
+}    }
+</script>
+<style>
+
+</style>
